e27decda892d39096ab02142e7740a4fb93f6326
pavanatm9909 <atmakuri.pavan9909@gmail.com>
2023-08-08 15:42:27 -0400

    first commit

---
+++ src/main/java/com/example/SpringPratice/SpringPraticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringPratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPraticeApplication.class, args);
+	}
+
+}
